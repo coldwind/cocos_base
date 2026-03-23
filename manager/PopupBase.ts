409f9fad0eee8810d4f0e8083ae70e84f7b10ac6
machao <vegeta.ma@163.com>
2026-03-24 00:10:20 +0800

    up

--- manager/PopupBase.ts
+++ manager/PopupBase.ts
@@ -1,17 +1,20 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component } from "cc";
+import { YPopupHandle } from "./PopupManager";
 const { ccclass, property } = _decorator;
 
-@ccclass('PopupBase')
+@ccclass("PopupBase")
 export class PopupBase extends Component {
-    protected popupInitData: any = null;
+  protected popupInitData: any = null;
 
-    public setInitData(data: any) {
-        this.popupInitData = data;
-    }
+  public setInitData(data: any) {
+    this.popupInitData = data;
+  }
 
-    public getInitData(): any {
-        return this.popupInitData;
-    }
+  public getInitData(): any {
+    return this.popupInitData;
+  }
+
+  public onClickClose() {
+    YPopupHandle.close();
+  }
 }
-
-
